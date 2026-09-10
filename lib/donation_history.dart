0f17632dc3a72ec5fcba844f537177a4ace3f1df
Nowshin <nowshin2233imtiaz@gmail.com>
2026-09-10 23:30:26 +0600

    removed codes from home.dart, donate_blood.dart, request_blood_screen.dart, , SearchDonors.dart , give_life_models.dart,

--- lib/donation_history.dart
+++ lib/donation_history.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'settings.dart';
 import 'SearchDonors.dart';
 import 'home.dart';
-import 'donate_blood.dart';
+//import 'donate_blood.dart';
 void main() {
   runApp(
     const MaterialApp(
@@ -218,9 +218,9 @@ class DonationHistory extends StatelessWidget {
             Navigator.push(context, MaterialPageRoute(builder: (context)=>const SearchDonors(),),);
           }
 
-          else if(index==2){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>const DonateBloodScreen(),),);
-          }
+          // else if(index==2){
+          //   Navigator.push(context, MaterialPageRoute(builder: (context)=>const DonateBloodScreen(),),);
+          // }
 
           else if(index==3){
             Navigator.push(context, MaterialPageRoute(builder: (context)=>const DonationHistory(),),);
